fdee6033e1e7dc5c9860e44c8bf7529f8cc1e55d
bareca90 <bareca90@gmail.com>
2023-08-18 10:34:27 -0500

    Validacion Conexión a la base de Datos

---
+++ app.js
@@ -0,0 +1,40 @@
+const sql       =   require('mssql/msnodesqlv8');
+const cron      =   require('node-cron');
+const config    =   require('./config/config');
+
+//--------------------------------------------
+//Validacion que se COnecte a la BD
+//--------------------------------------------
+const mensaje = async()=>{
+    try{
+        const respuesta = await sql.connect(config)
+        if (respuesta) console.log('Se Conecto a la Base Correctamente')
+        return true ;
+    }catch(error){
+        console.log(error)
+        return false ;
+    }
+}
+const main = async () => {
+    //--------------------------------------------
+    //Validación Base de Datos
+    //--------------------------------------------
+    const val_conexion = await mensaje();
+    if (val_conexion){
+        //--------------------------------------------
+        //Ejecución de el Cron de Consulta
+        //--------------------------------------------
+        try {
+            console.log('Ingreso por el dato correcto');
+            //cron.schedule('* * * * *', processRecords);
+        } catch (error) {
+            console.error('Error al Ejecutar Procesos:', error);
+        }
+        
+    }else{
+        console.log('Revise configuracion de BD');
+    }
+
+};
+
+main();
